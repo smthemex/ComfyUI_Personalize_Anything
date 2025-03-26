57b3421de301bf38f51a6e638c3bc82b20e6b8e9
smthemex <138738845+smthemex@users.noreply.github.com>
2025-03-26 08:34:31 +0800

    Update gradio_demo.py

--- gradio_demo.py
+++ gradio_demo.py
@@ -97,7 +97,7 @@ def generate_image_out(pipe,prompt,new_prompt, seed, timestep, tau, init_image,m
 
     return image
 
-def generate_image_in(pipe,prompt,new_prompt, seed, timestep, tau, init_image,mask,height,width,img_dims,device,shift,cf_clip=None):
+def generate_image_in(pipe,prompt, seed, timestep, tau, init_image,mask,height,width,img_dims,device,shift,cf_clip=None):
     shift_mask = shift_tensor(mask, shift) 
     generator = torch.Generator(device=device).manual_seed(seed)
 
@@ -105,10 +105,9 @@ def generate_image_in(pipe,prompt,new_prompt, seed, timestep, tau, init_image,ma
  
         inv_prompt_embeds,inv_pooled_prompt_embeds,inv_text_ids=cf_prompt_clip(cf_clip,"")
         inf_prompt_embeds_,inf_pooled_prompt_embeds_,_= cf_prompt_clip(cf_clip,prompt)
-        new_prompt_embeds,new_pooled_prompt_embeds,_=cf_prompt_clip(cf_clip,new_prompt)
-
-        pipe_prompt_embeds=torch.cat([inf_prompt_embeds_,new_prompt_embeds],dim=0)# batch size 2
-        pipe_pooled_prompt_embeds=torch.cat([inf_pooled_prompt_embeds_,new_pooled_prompt_embeds],dim=0)
+        
+        pipe_prompt_embeds=torch.cat([inf_prompt_embeds_,inv_prompt_embeds],dim=0)# batch size 2
+        pipe_pooled_prompt_embeds=torch.cat([inf_pooled_prompt_embeds_,inv_pooled_prompt_embeds],dim=0)
        
         cf_clip=None
         cf_unload()
